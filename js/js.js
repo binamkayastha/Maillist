087b7d6e2fd608f75b794890c92049311f24f235
binamkayastha <binamkayastha@gmail.com>
2015-10-10 11:09:20 -0400

    Program done for merging mailing list with techsync

--- js/js.js
+++ js/js.js
@@ -2,5 +2,29 @@ function create() {
     var mailValue = document.getElementById('maillist').value;
     var techValue = document.getElementById('techsync').value;
     var result = "MailList: " + mailValue + "\nTechValues: " + techValue;
+    //document.getElementById('final').value = result;
+    var splitMailValue = mailValue.split("\n");
+    var splitTechValue = techValue.split("\n");
+    //splitMailValue.sort();
+    //splitTechValue.sort();
+
+    var diff = $(splitTechValue).not(splitMailValue).get();
+    diff.sort();
+    var result = ""
+    diff.forEach(function(s) {
+        result += s;
+        result += "\n";
+    });
+
     document.getElementById('final').value = result;
+
+    var organized = splitMailValue.concat(diff);
+    organized.sort()
+    result = "";
+    organized.forEach(function(s) {
+        result += s;
+        result += "\n";
+    });
+
+    document.getElementById('organized').value = result;
 }
